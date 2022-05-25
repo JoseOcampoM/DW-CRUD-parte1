0ebf5381dd520d349202ad889edfc0cf09c1fccd
JoseOcampoM <jocampo@uniguajira.edu.co>
2022-05-25 04:12:52 -0500

    actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/layout/nav/nav.component';
+import { ListUsuarioComponent } from './components/usuario/list-usuario/list-usuario.component';
+import { ListMarcaComponent } from './components/marca/list-marca/list-marca.component';
+import { CreateUsuarioComponent } from './components/usuario/create-usuario/create-usuario.component';
+import { ListVehiculoComponent } from './components/vehiculo/list-vehiculo/list-vehiculo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ListUsuarioComponent,
+    ListMarcaComponent,
+    CreateUsuarioComponent,
+    ListVehiculoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
